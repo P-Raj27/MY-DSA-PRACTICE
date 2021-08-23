91e265a5cd49d844de6536ed26666b27f6106d6d
Manu Sharma <50588006+SharmaMania09@users.noreply.github.com>
2021-08-23 21:16:50 +0530

    Add files via upload

---
+++ Sort_An_Array_0's_1's_MS.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        int low=0, mid=0, high = n-1;
+        while(mid<=high)
+        {
+            if(a[mid]==0)
+            {
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
+// { Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+  // } Driver Code Ends
