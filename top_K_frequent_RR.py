1412fc11472170f8fdbe311cf07a68b97d537039
Rohit Roshan <rohit.roshan5@gmail.com>
2021-08-26 23:47:50 +0530

    Update top_K_frequent_RR.py

--- top_K_frequent_RR.py
+++ top_K_frequent_RR.py
@@ -1,8 +1,12 @@
 # SOLUTION USING HASHMAP
-
-#There can be solution using heap as well but have to study 
+IMPORTANT
+#There can be solution using HEAP  as well but have to study 
 #heap solution will be updated soon
+# another way to solve is  Quickselect (Hoare's selection algorithm) is used to (find kth something) but very complicated so heap is the best way (HEAP )
+# https://leetcode.com/problems/top-k-frequent-elements/solution/
 
+
+### MY SOLUTION IS USING HASH ##
 class Solution:
 	def topK(self, nums, k):
 	    
