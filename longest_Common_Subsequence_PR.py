1f83351bbe5bf39e1f3311e842b0328a7e8d57e0
P-Raj27 <85934814+P-Raj27@users.noreply.github.com>
2022-01-17 13:20:23 +0530

    Create longest_Common_Subsequence_PR.py
    This Can be done by using both recursion and Dynamic Programming.
    So DP uses the same logic as recursion but storing all the previous result as to enhance the speed.
    For Explanation You can visit this Youtube Link:(https://www.youtube.com/watch?v=ASoaQq66foQ)

---
+++ longest_Common_Subsequence_PR.py
@@ -0,0 +1,17 @@
+#Longest common subsequence by using Dynamic Programming.
+string1=input()
+string2=input()
+l1=len(string1)
+l2=len(string2)
+array=[[None]*(l2+1) for x in range(l1+1)]
+def lcs(string1,string2,l1,l2):
+    for i in range(l1+1):
+        for j in range(l2+1):
+            if(i==0 or j==0):
+                array[i][j]=0
+            elif(string1[i-1]==string2[j-1]):
+                array[i][j]=1+array[i-1][j-1]
+            else:
+                array[i][j]=max(array[i-1][j],array[i][j-1])
+    return array[l1][l2]
+print(lcs(string1,string2,l1,l2))
